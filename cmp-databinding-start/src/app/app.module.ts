f4c9de3d3259baf1137cdb7d534cc65ffd93d094
Richa Gupta <86804513+ravencode69@users.noreply.github.com>
2023-12-16 17:38:58 +0530

    C2_setupsInitial

--- cmp-databinding-start/src/app/app.module.ts
+++ cmp-databinding-start/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { ServerComponentComponent } from './server-component/server-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ cmp-databinding-start/src/app/server-component/server-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-component',
+  templateUrl: './server-component.component.html',
+  styleUrl: './server-component.component.css'
+})
+export class ServerComponentComponent {
+
+}
