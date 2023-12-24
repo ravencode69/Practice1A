b9aff0b45d4e80cab0c3adbad1dd3ce3f161ceba
Richa Gupta <86804513+ravencode69@users.noreply.github.com>
2023-12-24 19:34:54 +0530

    C14_usingHostBinding

--- directives-start/src/app/betterhighlight/betterhighlight.directive.ts
+++ directives-start/src/app/betterhighlight/betterhighlight.directive.ts
@@ -1,21 +1,35 @@
-import { Directive, ElementRef, Renderer2, OnInit, HostListener } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  OnInit,
+  HostListener,
+  HostBinding,
+} from '@angular/core';
 
 @Directive({
-  selector: '[appBetterhighlight]'
+  selector: '[appBetterhighlight]',
 })
 export class BetterhighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  // HostBinding is just another way of accessing the DOM (and manipulating it).
+  // It's an indirect way if you want to call it like that.
+  // Renderer gives you methods you can use to manipulate the DOM but you will
+  //actually rarely use it directly. HostBinding uses the renderer behind the scenes
+  // but is closer to the core idea of Angular: Declarative updating.
 
-  constructor(private eleRef: ElementRef, private render:Renderer2) { }
-  ngOnInit()
-  { 
- //   this.render.setStyle(this.eleRef.nativeElement,'background-color','lightblue');
-  }
-  
-  @HostListener('mouseenter',['$event']) mouseover(eventData:Event){
-  this.render.setStyle(this.eleRef.nativeElement,'background-color','lightblue');
+  constructor(private eleRef: ElementRef, private render: Renderer2) {}
+  ngOnInit() {
+    //   this.render.setStyle(this.eleRef.nativeElement,'background-color','lightblue');
   }
 
-  @HostListener('mouseleave',['$event']) mouseaway(event2:Event){
-    this.render.setStyle(this.eleRef.nativeElement,'background-color','transparent')
+  @HostListener('mouseenter', ['$event']) mouseover(eventData: Event) {
+    //this.render.setStyle(this.eleRef.nativeElement,'background-color','lightblue');
+    this.backgroundColor = 'lightblue';
+  }
+
+  @HostListener('mouseleave', ['$event']) mouseaway(event2: Event) {
+    // this.render.setStyle(this.eleRef.nativeElement,'background-color','transparent')
+    this.backgroundColor = 'transparent';
   }
 }
