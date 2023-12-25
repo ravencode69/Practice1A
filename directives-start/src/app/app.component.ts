21a2c2dcb3ba7d172a5ac76381619a406b221bef
Richa Gupta <86804513+ravencode69@users.noreply.github.com>
2023-12-25 14:46:05 +0530

    C17_ngSwitchUse

--- directives-start/src/app/app.component.ts
+++ directives-start/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   // numbers = [1, 2, 3, 4, 5];
-  public evenNumbers=[2,4];
-  oddNumbers=[1,3,5];
+  public evenNumbers = [2, 4];
+  oddNumbers = [1, 3, 5];
   onlyOdd = false;
+  value = 100;
 }
