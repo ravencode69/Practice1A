5d1afcb22b3223e2e947572636522adf84e72aee
Richa Gupta <86804513+ravencode69@users.noreply.github.com>
2024-01-01 22:26:35 +0530

    C20_sevicesDI_Notes

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -8,7 +8,11 @@ import { LoggerService } from '../logger.service';
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
 
+  //preferring a dependency injector, because it is more flexible and easier to test and
+  //maintain than the alternative of creating the service instance directly.
   constructor(private loggerOfStatus: LoggerService) {}
+  //above is a typescript shortcut for adding an accessor in front of the argument to
+  //instantly create a property with same name and store the incoming value in it.
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
